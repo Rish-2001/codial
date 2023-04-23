01a9d84905f4053170b6f1489702c05c558e7bf3
Rish-2001 <121566349+Rish-2001@users.noreply.github.com>
2023-04-23 13:18:40 +0530

    post is using by ajax without refreshing the page

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -9,7 +9,9 @@
                 url:'/posts/create',
                 data:newPostsForm.serialize(),
                 success:function(data){
-             
+                
+                    let newPost=newPostDom(data.data.post);
+                    $('#posts-list-container').prepend(newPost);
                     console.log(data);
                 },error: function(error){
                     console.log(error.responseText);
@@ -17,5 +19,39 @@
             })
         });
     }
+
+    let newPostDom = function(post){
+        return $(`<li id="post-${post._id}">
+                    <p>
+                        
+                        <small>
+                            <a class="delete-post-button"  href="/posts/destroy/${ post.id }">X</a>
+                        </small>
+                       
+                        ${ post.content }
+                        <br>
+                        <small>
+                        ${ post.user.name }
+                        </small>
+                    </p>
+                    <div class="post-comments">
+                        
+                            <form action="/comments/create" method="POST">
+                                <input type="text" name="content" placeholder="Type Here to add comment..." required>
+                                <input type="hidden" name="post" value="${ post._id }" >
+                                <input type="submit" value="Add Comment">
+                            </form>
+               
+                
+                        <div class="post-comments-list">
+                            <ul id="post-comments-${ post._id }">
+                                
+                            </ul>
+                        </div>
+                    </div>
+                    
+                </li>`)
+    }
+
     createPost();
 }
