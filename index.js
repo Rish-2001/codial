fd196e8ffb2eb6c798654b342e371f8dbaf44e79
Rish-2001 <121566349+Rish-2001@users.noreply.github.com>
2023-04-29 00:54:54 +0530

    upload the image

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ app.use(express.urlencoded());
 app.use(cookieParser());
 
 app.use(express.static('./assets'));
+//make the upload path
+app.use('/uploads',express.static(__dirname+'/uploads'));
 
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
