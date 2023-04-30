02ee1f48e8acfd937cf35656396c23881a6c33ce
Rish-2001 <121566349+Rish-2001@users.noreply.github.com>
2023-04-30 09:27:03 +0530

    post and comment deleted via api

--- controllers/api/v1/post_api.js
+++ controllers/api/v1/post_api.js
@@ -1,5 +1,5 @@
 const Post=require('../../../models/post');
-
+const Comment=require('../../../models/comment');
 module.exports.index=async function(req,res){
     
     let posts= await Post.find({}).populate('user')
@@ -15,4 +15,46 @@ module.exports.index=async function(req,res){
         message:"List of posts",
         posts:posts
     })
+}
+
+
+
+module.exports.destroy = async function(req, res){
+    try{
+    
+     let post=await Post.findById(req.params.id);
+     
+    // if (post.user == req.user.id){
+     post.remove();
+ 
+     await Comment.deleteMany({post: req.params.id});
+ 
+    //  if (req.xhr){
+    //    return res.status(200).json({
+    //        data: {
+    //            post_id: req.params.id
+    //        },
+    //        message: "Post deleted"
+    //    });
+//    }
+ 
+ 
+    //  req.flash('success','Post and associated comments deleted');
+     return res.json(200,{
+        message:"post and associated comment deleted successfully"
+     });
+     
+    // }
+    // else{
+    //  req.flash('error','You cannot delete this post ');
+    //          return res.redirect('back');
+    //      }
+    } catch(err){
+     
+        console.log('*******',err);
+    //  req.flash('error',err);
+      return res.json(500,{
+        message:"internal server error"
+      });
+    }
 }
