ba2872a752d3779a11ee14d4ed8b6e47cc8ca62a
Rish-2001 <121566349+Rish-2001@users.noreply.github.com>
2023-08-11 09:54:04 +0530

    changes in post and comment

--- models/post.js
+++ models/post.js
@@ -22,5 +22,5 @@ const postSchema = new mongoose.Schema({
     timestamps: true
 });
 
-const Post = mongoose.model('Post', postSchema);
+const Post = mongoose.model('post', postSchema);
 module.exports = Post;
